80dfa31201fb3efbf376bfe0bceb9907d7896a34
18032002 <l.vergara1803@gmail.com>
2026-05-06 22:44:18 -0500

    Completed exercise 02 double or triple

---
+++ src/02_double_or_triple.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if num1 >= num2:
+    print("Result:", num1 * 2)
+else:
+    print("Result:", num2 * 3)
+    
